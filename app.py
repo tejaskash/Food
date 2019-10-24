da671102b01d7f8a4575139b07b55cfb16b12d65
tejaskashinathofficial <tejas.hariharan@learner.manipal.edu>
2019-10-24 14:47:17 +0530

    Removed oAuth

--- app.py
+++ app.py
@@ -22,16 +22,7 @@ app.register_blueprint(google_bp, url_prefix="/login")
 app.register_blueprint(reg)
 oauth = OAuth()
 
-google = oauth.remote_app('google',
-                           base_url='https://www.google.com/accounts/',
-                           authorize_url='https://accounts.google.com/o/oauth2/auth',
-                           request_token_url=None,
-                           request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email','response_type': 'code'},
-                           access_token_url='https://accounts.google.com/o/oauth2/token',
-                           access_token_method='POST',
-                           access_token_params={'grant_type': 'authorization_code'},
-                           consumer_key=GOOGLE_CLIENT_ID,
-                           consumer_secret=GOOGLE_CLIENT_SECRET)
+
 @app.errorhandler(405)
 def error405(code):
     print(code)
@@ -42,27 +33,6 @@ def mainPage():
 @app.route("/loginPage" ,methods=['GET','POST'])
 def loginPage():
     return render_template("login.html")
-@app.route("/login/google",methods=['GET','POST'])
-def index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('login'))
-    access_token = access_token[0]
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',None, headers)
-    try:
-        res = urlopen(req)
-    except URLError as e:
-        if e.code == 401:
-            session.pop('access_token',None)
-            return redirect(url_for('login'))
-        return res.read()
-    data = res.read()
-    data = data.decode('utf8')
-    jsondata = json.loads(data)
-    LOGGED_IN_USER_EMAIL = jsondata["email"]
-    #TODO: ADD Code to Check if user in DB, if yes then redirect to Main Page, else redirect to register page
-    return redirect(url_for("mainPage"))
 @app.route("/login")
 def login():
     callback = url_for('authorized', _external = True)
