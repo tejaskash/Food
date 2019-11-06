030c1f44e3d9650607982eaafa69a7f4bb96fa26
tejaskashinathofficial <tejas.hariharan@learner.manipal.edu>
2019-11-06 20:17:22 +0530

    GM BSDK

--- orders.py
+++ orders.py
@@ -1,7 +1,10 @@
 from flask import Blueprint,render_template
-
+from pymongo import MongoClient
 order = Blueprint("order","order")
-
+client = MongoClient("mongodb://localhost:27017")
 @order.route('/order/<name>')
 def displayPage(name):
-    return "<h3>"+name+"<h3>"
+    db = client.restdb
+    res = db.rest.find_one({"name":name})
+    print(res)
+    return render_template("restaurant.html",name=name,data=res)
