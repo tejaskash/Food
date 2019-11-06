abb894b8617ec5711bc79ca9389da222a5021adb
tejaskashinathofficial <tejas.hariharan@learner.manipal.edu>
2019-11-06 17:05:18 +0530

    bleh

--- profile.py
+++ profile.py
@@ -12,5 +12,17 @@ def profilePage():
     fname=res["fname"]
     lname=res["lname"]
     db=client.addressdb
-    res=db.userAddress
-    return render_template("profile.html",fname=fname,lname=lname,email=email)
+    res=db.userAddress.find_one({"email":email})
+    return render_template("profile.html",fname=fname,lname=lname,email=email,unum=res["unum"],bname=res["bname"],addr1=res["addr1"],addr2=res["addr2"],city=res["city"],pincode=res["pincode"])
+@prof.route("/profile/edit/<email>")
+def editProfile(email):
+    db = client.logindb
+    res = db.loggedin.find_one()
+    email = res['user']
+    db = client.userdb
+    res = db.userDetails.find_one({"email":email})
+    fname=res["fname"]
+    lname=res["lname"]
+    db=client.addressdb
+    res=db.userAddress.find_one({"email":email})
+    return render_template("EditProfile.html",fname=fname,lname=lname,email=email,unum=res["unum"],bname=res["bname"],addr1=res["addr1"],addr2=res["addr2"],city=res["city"],pincode=res["pincode"])
