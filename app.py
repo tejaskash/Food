54fd721b0ff8c8e55a3311347e7c52964d4def55
tejaskashinathofficial <tejas.hariharan@learner.manipal.edu>
2019-11-06 15:14:10 +0530

    Bleh

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 import json
 import os
 
-client = MongoClient("mongodb+srv://tejas:1234@pocketsdb-i09wt.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb://localhost:27017")
 
 app = Flask(__name__)
 app.register_blueprint(reg)
@@ -16,24 +16,33 @@ def error405(code):
     return "<h1>Ooops!!, How did you get here??"
 @app.route("/")
 def mainPage():
-    return render_template("dashboard.html")
-@app.route("/loginPage" ,methods=['GET','POST'])
-def loginPage():
     return render_template("login.html")
 @app.route("/login/auth",methods=['POST'])
 def loginAuth():
-    user = request.form['user']
-    password = request.form['password']
+    user = request.form['email']
+    print(user)
+    password = request.form['psw']
+    print(password)
     db = client.logindb
-    res = db.loginAuth.find_one({""+user+"":""+password+""})
+    res=db.loginAuth.find_one({"user":user,"passw":password})
     print(res)
     if res == None:
         return "<h1>Invalid Password</h1>"
-    elif res[user] == password:
-        return redirect(url_for("mainPage"))
+    elif res["passw"] == password:
+        db.loggedin.insert_one({"user":user})
+        return render_template("dashboard.html")
     else:
         return "<h1>Invalid Password</h1>"
     return "<h1>Hello</h1>"
-    
+@app.route("/logout")
+def logout():
+    db = client.logindb
+    res=db.loggedin.find_one()
+    if(res!=None):
+        db.loggedin.delete_one(res)
+        return render_template("login.html")
+    else:
+        return "<h1>Not Logged In</h1>"
+
 if __name__ == "__main__":
     app.run("localhost",port=5000,threaded=True,debug=True)
--- register.py
+++ register.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,request
 from pymongo import MongoClient
-
+client=MongoClient("mongodb://localhost:27017")
 reg = Blueprint('register',__name__,template_folder='templates')
 @reg.route("/register",methods=['GET','POST'])
 def regr():
@@ -8,7 +8,6 @@ def regr():
 
 @reg.route("/onboarding",methods=['POST'])
 def onboarding():
-    client = MongoClient("mongodb+srv://tejas:1234@pocketsdb-i09wt.mongodb.net/test?retryWrites=true&w=majority")
     fname = str(request.form['fname'])
     lname = str(request.form['lname'])
     email = str(request.form['email'])
@@ -19,11 +18,9 @@ def onboarding():
     db.loginAuth.insert_one({"user":email,"passw":passw})
     db = client.emails
     db.emailDB.insert_one({"email":email})
-    client.close()
     return render_template("address.html")
 @reg.route("/address",methods=["GET","POST"])
 def address():
-    client = MongoClient("mongodb+srv://tejas:1234@pocketsdb-i09wt.mongodb.net/test?retryWrites=true&w=majority")
     unum = str(request.form['fname'])
     bname = str(request.form['lname'])
     addr1 = str(request.form['email'])
@@ -35,8 +32,7 @@ def address():
     db.emailDB.delete_one({"email":email})
     db = client.addressdb
     db.userAddress.insert_one({"email":email,"unum":unum,"bname":bname,"addr1":addr1,"addr2":addr2,"city":city,"pincode":pincode})
-    client.close()
-    return render_template("hello.html")
+    return render_template("dashboard.html")
 
 
 
